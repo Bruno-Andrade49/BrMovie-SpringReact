eaff591f1f09677905b58d9feac604d2adfcad9d
Bruno Andrade <b.emanueandrade@hotmail.com>
2022-01-10 11:03:53 -0300

    Projeto criado

---
+++ backend/src/main/java/com/brproj/bootcamp/projetospringreact/projetoSpringReact/ProjetoSpringReactApplication.java
@@ -0,0 +1,13 @@
+package com.brproj.bootcamp.projetospringreact.projetoSpringReact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSpringReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSpringReactApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/brproj/bootcamp/projetospringreact/projetoSpringReact/ProjetoSpringReactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brproj.bootcamp.projetospringreact.projetoSpringReact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoSpringReactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
