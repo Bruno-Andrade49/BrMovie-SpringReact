a14188d899891828c8aa36023cb7dfe750a304a0
Bruno Andrade <b.emanueandrade@hotmail.com>
2022-01-11 09:52:43 -0300

    Navbar implementada

---
+++ frontend/src/components/Navbar/index.tsx
@@ -0,0 +1,22 @@
+import {ReactComponent as GitHubIcon} from 'assets/img/Vector.svg';
+import "./style.css";
+
+function NavBar() {
+    return (
+        <header>
+          <nav className="container">
+            <div className="dsMovie-nav-content">
+              <h1>DsMovie</h1>
+              <a href="https://github.com/Bruno-Andrade49" target="_blank"> 
+                <div className="link-gitHub">
+                  <GitHubIcon/>
+                  <p> /brunoAndrade </p>
+                </div>
+              </a>
+            </div>
+          </nav>
+        </header>
+      );
+}
+
+export default NavBar;
