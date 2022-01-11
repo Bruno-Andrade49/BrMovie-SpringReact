d446574645ba31c5bb0dbdaee7c6762ec40f3039
Bruno Andrade <b.emanueandrade@hotmail.com>
2022-01-11 10:16:57 -0300

    Rotas implementadas

---
+++ frontend/src/pages/form/index.tsx
@@ -0,0 +1,7 @@
+function Form() {
+    return (
+        <h1> Pagina de formulário</h1>
+    )
+}
+
+export default Form;
---
+++ frontend/src/pages/listing/index.tsx
@@ -0,0 +1,7 @@
+function Listning() {
+    return (
+        <h1> Pagina de listagem</h1>
+    )
+}
+
+export default Listning;
