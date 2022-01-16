4e800833de3389f8bff31ed647be546d2145fde4
Bruno Andrade <b.emanueandrade@hotmail.com>
2022-01-16 11:50:24 -0300

    First request

--- frontend/src/pages/listing/index.tsx
+++ frontend/src/pages/listing/index.tsx
@@ -1,10 +1,14 @@
+import axios from "axios";
 import MovieCard from "components/MovieCard";
-import MovieScore from "components/MovieScore";
-import MovieStars from "components/movieStars";
 import Pagination from "components/Pagination";
+import { BASE_URL } from "utils/requests";
 
 
 function Listning() {
+
+    axios.get(`${BASE_URL}/movies`).then(response => { console.log(response.data)})
+
+
     return (
         <>
             <Pagination />
